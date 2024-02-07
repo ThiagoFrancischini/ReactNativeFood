e3ed2f931db417cebdc8c092caeafc8f45d66f0f
Thiago Schimitz <thiago.schimitz@softbyte.local>
2024-02-07 13:38:36 -0300

    primeiroCommit

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import {View, Text, StyleSheet} from "react-native"
+
+export default function Home(){
+    return (
+        <View>
+            <Text> Hello </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "red",
+        flex: 1
+    }
+});
