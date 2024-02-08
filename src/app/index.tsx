fcdee71ea36c6614c53ec30fdf3e959555b84a57
Thiago Schimitz <thiago.schimitz@softbyte.local>
2024-02-08 16:41:09 -0300

    Primeira aula

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,29 @@
-import {View, Text, StyleSheet} from "react-native"
+import {View, FlatList} from "react-native"
+import{ CATEGORIES } from "@/utils/data/products"
+import {Header } from "@/components/header"
+import { CategoryButton } from "@/components/category-button"
+import { useState } from "react"
 
 export default function Home(){
+
+    const [category, setCategory] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(selectedCategory : string){
+        setCategory(selectedCategory);
+    }
+
     return (
-        <View>
-            <Text> Hello </Text>
+        <View className="flex-1 pt-8">
+            <Header title="Faça o seu pedido" cartQuantityItems={5}/>            
+
+            <FlatList 
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({item}) => <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)}></CategoryButton>}
+                horizontal
+                className="max-h-10 mt-5"
+                contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+                showsHorizontalScrollIndicator={false}/>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "red",
-        flex: 1
-    }
-});
